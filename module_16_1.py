08582db79c70bd9a767e27293a8e959c7db3c9c1
Aleksey <moon41@yandex.ru>
2025-01-03 16:47:54 +0300

    Основы Fast Api и маршрутизация

--- module_16_1.py
+++ module_16_1.py
@@ -4,5 +4,20 @@ app = FastAPI()
 
 
 @app.get('/')
-async def root():
-    return {'message': 'Hello'}
+async def root() -> dict:
+    return {'message': 'Главная страница'}
+
+
+@app.get('/user/admin')
+async def admin() -> dict:
+    return {'message': 'Вы вошли как администратор'}
+
+
+@app.get('/user/{user_id}')
+async def user(user_id: int) -> dict:
+    return {'message': f'Вы вошли как пользователь № {user_id}'}
+
+
+@app.get('/user')
+async def user_info(user_name: str, user_age: int) -> dict:
+    return {'message': f'Информация о пользователе. Имя: {user_name}, Возраст: {user_age}'}
