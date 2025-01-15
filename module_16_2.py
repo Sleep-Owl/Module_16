42afdc33d7ac19f037feb0d05ef6c1a451336dee
Aleksey <moon41@yandex.ru>
2025-01-15 22:36:15 +0300

    Валидация данных

--- module_16_2.py
+++ module_16_2.py
@@ -15,16 +15,16 @@ async def admin() -> dict:
 
 
 @app.get('/user/{user_id}')
-async def user(user_id: int = Path(
+async def user(user_id: Annotated[int, Path(
     ge=1, le=100, description='Enter User ID', example='1'
-)) -> dict:
+)]) -> dict:
     return {'message': f'Вы вошли как пользователь № {user_id}'}
 
 
 @app.get('/user/{username}/{age}')
 async def user_info(username: Annotated[str, Path(
     min_length=5, max_length=20, description='Enter username', example='UrbanUser'
-)], age: int = Path(
+)], age: Annotated[int, Path(
     ge=18, le=120, description='Enter age', example='24'
-)) -> dict:
+)]) -> dict:
     return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
